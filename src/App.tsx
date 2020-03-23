86774fb66f7d1c3428ba24e73b765b816a19daf3
tenwiz <hoghtonlouis@gmail.com>
2020-03-23 10:34:04 -0500

    Mass updates with coding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import {useImageColor, usePastedImage} from "./hooks";
+import { useImageColor, usePastedImage } from "./hooks";
 import Dropzone from "./Dropzone";
 import Toolbar from "./Toolbar";
 
 const Wrapper = styled.div`
-	min-width: 400px;
+  min-width: 400px;
 `;
 
 /**
@@ -13,85 +13,83 @@ const Wrapper = styled.div`
  * and the result will not have rounded corners
  */
 const Border = styled.div`
-	border: 2px solid white;
-	border-radius: 4px;
-	overflow: hidden;
+  border: 2px solid white;
+  border-radius: 4px;
+  overflow: hidden;
 `;
 
-const Content = styled.div<{bg: string}>`
-	margin: auto;
-	padding: 10px;
-	background: ${p => p.bg};
-	padding: 64px;
-	box-sizing: border-box;
-	//resize: both;
-	//overflow: auto;
+const Content = styled.div<{ bg: string }>`
+  margin: auto;
+  padding: 10px;
+  background: ${p => p.bg};
+  padding: 64px;
+  box-sizing: border-box;
+  //resize: both;
+  //overflow: auto;
 `;
 
-const Window = styled.div<{bg: string}>`
-	background: ${p => p.bg};
-	border-radius: 10px;
-	box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.4);
-	min-height: 100px;
-	height: 100%;
-	padding: 8px;
-	display: flex;
-	flex-direction: column;
+const Window = styled.div<{ bg: string }>`
+  background: ${p => p.bg};
+  border-radius: 10px;
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.4);
+  min-height: 100px;
+  height: 100%;
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Header = styled.div`
-	display: flex;
-	margin-bottom: 8px;
+  display: flex;
+  margin-bottom: 8px;
 `;
 
-const Dot = styled.div<{color: string}>`
-	background: ${p => p.color};
-	width: 12px;
-	height: 12px;
-	border-radius: 50%;
-	margin: 4px;
+const Dot = styled.div<{ color: string }>`
+  background: ${p => p.color};
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  margin: 4px;
 `;
 
 export default () => {
-	const canvas = useRef<HTMLCanvasElement>(null);
-	const [image, setImage] = usePastedImage();
-	const imageColor = useImageColor(canvas?.current, image ?? null);
-	const [bgColor, setBgColor] = useState("cadetblue");
-	const [windowColor, setWindowColor] = useState("#263238");
-	const contentRef = useRef<HTMLDivElement>(null);
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [image, setImage] = usePastedImage();
+  const imageColor = useImageColor(canvas?.current, image ?? null);
+  const [bgColor, setBgColor] = useState("cadetblue");
+  const [windowColor, setWindowColor] = useState("#263238");
+  const contentRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		if (imageColor) setWindowColor(imageColor);
-	}, [imageColor]);
+  useEffect(() => {
+    if (imageColor) setWindowColor(imageColor);
+  }, [imageColor]);
 
-	return (
-		<Wrapper>
-			<Toolbar
-				{...{bgColor, windowColor, imageColor}}
-				content={contentRef.current}
-				onBgColor={setBgColor}
-				onWindowColor={setWindowColor}
-			/>
+  return (
+    <Wrapper>
+      <Toolbar
+        {...{ bgColor, windowColor, imageColor }}
+        content={contentRef.current}
+        onBgColor={setBgColor}
+        onWindowColor={setWindowColor}
+      />
 
-			<Border>
-				<Content bg={bgColor} ref={contentRef as any}>
-					<Window bg={windowColor}>
-						<Header>
-							<Dot color={"#FF6259"} />
-							<Dot color={"#FFBF2F"} />
-							<Dot color={"#29CE42"} />
-						</Header>
-						<Dropzone
-							hasImage={!!image}
-							onImage={image => setImage(image)}>
-							<canvas
-								ref={canvas}
-								style={{display: !!image ? "initial" : "none"}}
-							/>
-						</Dropzone>
-					</Window>
-				</Content>
-			</Border>
-		</Wrapper>
-	);
+      <Border>
+        <Content bg={bgColor} ref={contentRef as any}>
+          <Window bg={windowColor}>
+            <Header>
+              <Dot color={"#FF6259"} />
+              <Dot color={"#FFBF2F"} />
+              <Dot color={"#29CE42"} />
+            </Header>
+            <Dropzone hasImage={!!image} onImage={image => setImage(image)}>
+              <canvas
+                ref={canvas}
+                style={{ display: !!image ? "initial" : "none" }}
+              />
+            </Dropzone>
+          </Window>
+        </Content>
+      </Border>
+    </Wrapper>
+  );
 };
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,74 +1,74 @@
-import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 export const usePastedImage = (): [
-	HTMLImageElement | undefined,
-	Dispatch<SetStateAction<HTMLImageElement | undefined>>
+  HTMLImageElement | undefined,
+  Dispatch<SetStateAction<HTMLImageElement | undefined>>
 ] => {
-	const [image, setImage] = useState<HTMLImageElement>();
-	useEffect(() => {
-		const listener = (e: Event) => {
-			const {clipboardData: data} = e as ClipboardEvent;
-			const items = data?.items || [];
+  const [image, setImage] = useState<HTMLImageElement>();
+  useEffect(() => {
+    const listener = (e: Event) => {
+      const { clipboardData: data } = e as ClipboardEvent;
+      const items = data?.items || [];
 
-			for (let item of items) {
-				if (item.type.indexOf("image") == -1) continue;
-				const blob = item.getAsFile();
-				let img = new Image();
-				let URLObj = window.URL || window.webkitURL;
-				img.src = URLObj.createObjectURL(blob);
-				setImage(img);
-				return;
-			}
-		};
+      for (let item of items) {
+        if (item.type.indexOf("image") == -1) continue;
+        const blob = item.getAsFile();
+        let img = new Image();
+        let URLObj = window.URL || window.webkitURL;
+        img.src = URLObj.createObjectURL(blob);
+        setImage(img);
+        return;
+      }
+    };
 
-		window.addEventListener("paste", listener);
-		return () => window.removeEventListener("paste", listener);
-	}, []);
+    window.addEventListener("paste", listener);
+    return () => window.removeEventListener("paste", listener);
+  }, []);
 
-	return [image, setImage];
+  return [image, setImage];
 };
 
 export const useImageColor = (
-	canvas: HTMLCanvasElement | null,
-	image: HTMLImageElement | null
+  canvas: HTMLCanvasElement | null,
+  image: HTMLImageElement | null
 ) => {
-	const [color, setColor] = useState<string | null>(null);
-	useEffect(() => {
-		if (!image) return;
-		image.onload = () => {
-			if (!canvas || !image) return;
-			let ctx = canvas.getContext("2d");
-			if (!ctx) return;
+  const [color, setColor] = useState<string | null>(null);
+  useEffect(() => {
+    if (!image) return;
+    image.onload = () => {
+      if (!canvas || !image) return;
+      let ctx = canvas.getContext("2d");
+      if (!ctx) return;
 
-			const w = image.width;
-			const h = image.height;
+      const w = image.width;
+      const h = image.height;
 
-			// apply scaling based on pixel ratio?
-			// todo review this
-			canvas.width = w / window.devicePixelRatio;
-			canvas.height = h / window.devicePixelRatio;
+      // apply scaling based on pixel ratio?
+      // todo review this
+      canvas.width = w / window.devicePixelRatio;
+      canvas.height = h / window.devicePixelRatio;
 
-			(ctx as any).mozImageSmoothingEnabled = false;
-			(ctx as any).imageSmoothingEnabled = false;
-			ctx.drawImage(image, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
+      (ctx as any).mozImageSmoothingEnabled = false;
+      (ctx as any).imageSmoothingEnabled = false;
+      ctx.drawImage(image, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
 
-			let {data} = ctx.getImageData(0, 0, canvas.width, canvas.height);
-			const colors: {[key: string]: number} = {};
+      let { data } = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const colors: { [key: string]: number } = {};
 
-			for (let i = 0, n = data.length; i < n; i += 4) {
-				const key = data.slice(i, i + 4).join(",");
-				colors[key] = colors[key] + 1 || 1;
-			}
-			const color = Object.entries(colors).reduce(
-				(a, [key, value]) => {
-					if (value > a[1]) return [key, value];
-					return a;
-				},
-				["", 0]
-			)[0];
+      for (let i = 0, n = data.length; i < n; i += 4) {
+        const key = data.slice(i, i + 4).join(",");
+        colors[key] = colors[key] + 1 || 1;
+      }
+      const color = Object.entries(colors).reduce(
+        (a, [key, value]) => {
+          if (value > a[1]) return [key, value];
+          return a;
+        },
+        ["", 0]
+      )[0];
 
-			setColor(`rgba(${color})`);
-		};
-	}, [image]);
-	return color;
+      setColor(`rgba(${color})`);
+    };
+  }, [image]);
+  return color;
 };
